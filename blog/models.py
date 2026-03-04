10f4027134e4e93f85c5ee1ccd857c31c07d751b
michxdr <nikitakorpan9@gmail.com>
2026-03-04 14:29:02 +0200

    Initial setup: models, views, auth

---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.conf import settings
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Чернетка'),
+        ('published', 'Опубліковано'),
+    ]
+
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+    body = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(status='published')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'blog/post_detail.html', {'post': post})

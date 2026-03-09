869068043e04f770bcc2f136c8422d7333017fec
michxdr <nikitakorpan9@gmail.com>
2026-03-09 19:29:13 +0200

    Automatic slug creation

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,5 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'slug', 'body', 'category', 'tags', 'status']
+        fields = ['title', 'body', 'category', 'tags', 'status']
 
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -34,6 +35,17 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title, allow_unicode=True)
+            slug = base_slug
+            n = 1
+            while Post.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f'{base_slug}-{n}'
+                n += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title

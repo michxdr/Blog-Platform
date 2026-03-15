b9648e0c8b90de741fc974eff083f5150faef57d
michxdr <nikitakorpan9@gmail.com>
2026-03-15 14:04:02 +0200

    Add pagination, search, filtering

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('create/', views.post_create, name='post_create'),
+    path('search/', views.post_search, name='post_search'),
+    path('category/<slug:slug>/', views.post_by_category, name='post_by_category'),
+    path('tag/<slug:slug>/', views.post_by_tag, name='post_by_tag'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit/', views.post_edit, name='post_edit'),
     path('<slug:slug>/delete/', views.post_delete, name='post_delete'),
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.forms import PostForm
-from blog.models import Post
+from blog.models import Post, Category, Tag
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 def post_list(request):
     posts = Post.objects.filter(status='published')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'posts': page_obj})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
@@ -47,4 +51,26 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_create.html', {'form': form})
+
+
+def post_by_category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    posts = Post.objects.filter(status='published', category=category)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_by_tag(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    posts = Post.objects.filter(status='published', tags=tag)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_search(request):
+    query = request.GET.get('q')
+    if query:
+        posts = Post.objects.filter(status='published', title__icontains=query)
+        return render(request, 'blog/post_list.html', {'posts': posts})
+    else:
+        return redirect('post_list')
+
 
